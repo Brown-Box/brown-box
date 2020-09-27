51e89f124b0460a780b36a02a4cd9146eb971550
MachichM <martindobrovolny@seznam.cz>
2020-09-27 12:56:01 +0200

    GA support for categorical

--- brown_box/meta_optimizers/geneticSearch.py
+++ brown_box/meta_optimizers/geneticSearch.py
@@ -2,7 +2,8 @@ from time import time
 from typing import Optional
 
 import numpy as np
-from scipy.optimize._differentialevolution import DifferentialEvolutionSolver
+from scipy.optimize._differentialevolution import DifferentialEvolutionSolver, _ConstraintWrapper, warnings, \
+    MapWrapper, _FunctionWrapper, Bounds, check_random_state, string_types, new_bounds_to_old
 
 from brown_box_package.brown_box.utils.hyper_transformer import HyperTransformer
 
@@ -16,15 +17,22 @@ class GeneticSearch:
         self._timeout = None
         self._timeout_passed = None
 
+    # TODO move fit function here
     def search(self, seed: int = 42, timeout: Optional[int] = None) -> dict:
         bounds = []
         for param_name, param_info in self._api_config.items():
-            if param_info["type"] not in ["real", "int"]:
+            if param_info["type"] in ["real", "int"]:
+                kwargs = {param_name: param_info["range"]}
+                real_range = self._transformer.to_real_space(**kwargs)
+                bounds.append([val[0] for val in real_range])
+            elif param_info["type"] == "cat":
+                bounds.append((0, 1))
+            elif param_info["type"] == "bool":
+                kwargs = {param_name: param_info["range"]}
+                raise NotImplemented()
+            else:
                 param_type = param_info["type"]
-                raise ValueError(f"Unsupported type of parameter {param_type}")
-            kwargs = {param_name: param_info["range"]}
-            real_range = self._transformer.to_real_space(**kwargs)
-            bounds.append((real_range[0][0], real_range[1][0]))
+                raise ValueError(f"Unknown parameter type {param_type}")
 
         if timeout:
             self._start_time = time()
@@ -34,8 +42,11 @@ class GeneticSearch:
         else:
             callback = None
 
-        solver = DifferentialEvolutionSolver(
-            self._fit_function, bounds=bounds, seed=seed, callback=callback
+        func = self._GA_to_real_wrapper(self._fit_function)
+
+        # TODO turn on polish? Find out our own way to polish result?
+        solver = BrownEvolutionSolver(
+            transformer=self._transformer, func=func, bounds=bounds, seed=seed, callback=callback, polish=False
         )
         solver_return_value = solver.solve()
         if not solver_return_value["success"] and not self._timeout_passed:
@@ -45,11 +56,13 @@ class GeneticSearch:
             )
 
         best_value = solver.x
-        real_result = [
-            [best_value[i]] for i in range(len(self._api_config.keys()))
-        ]
-        result = self._transformer.to_hyper_space(np.array(real_result))
-        result = {key: val[0][0] for key, val in result.items()}
+        real_params = self._GA_to_real(best_value)
+        # real_params = [np.array(val) if type(val) is not list else val for val in real_params]  # TODO je to potreba?
+        real_params = np.stack(real_params)
+        if len(real_params.shape) == 1:
+            real_params = np.array([real_params])
+        result = self._transformer.to_hyper_space(real_params)
+        result = {key: val[0] for key, val in result.items()}
 
         return result
 
@@ -59,3 +72,72 @@ class GeneticSearch:
             return True
         else:
             return False
+
+    def _GA_to_real_wrapper(self, func):
+        def transform(population_member):
+            real_params = self._GA_to_real(population_member)
+            return func(real_params)
+        return transform
+
+    def _GA_to_real(self, population_member):
+        real_params = []
+        for i, (param_name, param_info) in enumerate(self._api_config.items()):
+            if param_info["type"] == "real":
+                value = population_member[i]
+            # TODO ask Kuba how to process int. Is rounding right?
+            elif param_info["type"] == "int":
+                value = int(round(population_member[i]))
+            elif param_info["type"] == "cat":
+                number_of_categories = len(param_info["values"])
+                cat_pos = min(int(population_member[i] * number_of_categories), number_of_categories)
+                kwargs = {param_name: [param_info["values"][cat_pos]]}
+                value = self._transformer.to_real_space(**kwargs)[0]
+            elif param_info["type"] == "bool":
+                raise NotImplemented()
+            else:
+                raise ValueError()
+            real_params.append(value)
+        return real_params
+
+
+class BrownEvolutionSolver(DifferentialEvolutionSolver):
+    # def __init__(self, transformer, *args, **kwargs):
+    #     self.transformer = transformer
+    #   super().__init__(*args, **kwargs)
+
+    def __init__(self, transformer, func, bounds, args=(),
+                 strategy='best1bin', maxiter=1000, popsize=15,
+                 tol=0.01, mutation=(0.5, 1), recombination=0.7, seed=None,
+                 maxfun=np.inf, callback=None, disp=False, polish=True,
+                 init='latinhypercube', atol=0, updating='immediate',
+                 workers=1, constraints=()):
+        super().__init__(func, bounds, args,
+                 strategy, maxiter, popsize,
+                 tol, mutation, recombination, seed,
+                 maxfun, callback, disp, polish,
+                 init, atol, updating,
+                 workers, constraints)
+        self.transformer = transformer
+
+    def _best1(self, samples):
+        """best1bin, best1exp"""
+        r0, r1 = samples[:2]
+        new_pop_member = (self.population[0] + self.scale *
+                         (self.population[r0] - self.population[r1]))
+        for i, (param_name, param_info) in enumerate(self.transformer.api_config.items()):
+            if param_info["type"] in ["cat"]:
+                number_of_categories = len(param_info["values"])
+                pop0_cat, r0_cat, r1_cat = [min(int(self.population[i] * number_of_categories), number_of_categories) for i in [0, r0, r1]]
+                if r0_cat != r1_cat:
+                    random_number = self.random_number_generator.random_sample(1)[0]
+                    if random_number < self.scale:
+                        if pop0_cat != r0_cat:
+                            new_pop_member[i] = self.population[r0]
+                        else:
+                            new_pop_member[i] = self.population[r1]
+                    else:
+                        new_pop_member[i] = self.population[0]
+            elif param_info["type"] == "bool":
+                raise NotImplementedError()
+
+        return new_pop_member
--- brown_box/meta_optimizers/test/test_geneticSearch.py
+++ brown_box/meta_optimizers/test/test_geneticSearch.py
@@ -1,17 +1,32 @@
 import pytest
 from time import time
 
+import numpy as np
 
 from brown_box_package.brown_box.meta_optimizers.geneticSearch import GeneticSearch
 from brown_box_package.brown_box.utils.hyper_transformer import HyperTransformer
 
-
-def basic_fit_function(x):
+# TODO toto zere list
+def basic_fit_function(values):
+    x = values[0]
     return 2 * x ** 2 + 4 * x + 2
 
 
-def basic_fit_function_10(x):
+def basic_fit_function_10(values):
+    x = values[0]
     return abs(x - 1)
+
+
+def cat_fit_function(values):
+    cat_value = values[0]
+    if np.array_equal([1., 0., 0.], cat_value):
+        return 0
+    elif np.array_equal([0., 1., 0.], cat_value):
+        return 1
+    elif np.array_equal([0., 0., 1.], cat_value):
+        return 2
+    else:
+        raise ValueError(f"Bad value of x_real: {values}")
 
 
 def test_happy_path():
@@ -35,7 +50,7 @@ def test_timeout_callback():
     proposal = gs.search(timeout=0.001)
     end_time = time()
 
-    assert pytest.approx(-1, 0.1) == proposal["x"]
+    assert pytest.approx(-1, 1) == proposal["x"]
     assert end_time - start_time < 0.1
 
 
@@ -48,3 +63,17 @@ def test_log_space():
     proposal = gs.search()
 
     assert pytest.approx(10, 0.1) == proposal["x"]
+
+
+def test_categorical():
+    api_config = {
+        "f": {"type": "cat", "values": ["exp", "log", "abs"]},
+    }
+    transformer = HyperTransformer(api_config)
+    gs = GeneticSearch(transformer, cat_fit_function)
+    proposal = gs.search()
+
+    assert "exp" == proposal["f"]
+
+
+# test cat + real
