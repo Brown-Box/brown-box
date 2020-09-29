ad47b97dc158a6a0a188d7ffe522490dcf7eaa8f
Jakub Klus <jakub.klus@innovatrics.com>
2020-09-29 05:41:54 +0000

    Add proper reshape to avoid exception

--- brown_box/cost_functions/probability_of_improvement.py
+++ brown_box/cost_functions/probability_of_improvement.py
@@ -12,7 +12,7 @@ def poi(gp, tr, min_y=0, xi=0.1, **_):
 
 def poi_real(gp, tr, min_y=0, xi=0.1, **_):
     def cost(X):
-        mean, std = gp.predict(X.reshape(-1, ), return_std=True)
+        mean, std = gp.predict(X.reshape(1, -1), return_std=True)
         z = (mean + min_y + xi) / std
         return norm.cdf(z)
 
--- brown_box/cost_functions/upper_confidence_bound.py
+++ brown_box/cost_functions/upper_confidence_bound.py
@@ -9,7 +9,7 @@ def ucb(gp, tr, kappa=2.6, **_):
 
 def ucb_real(gp, tr, kappa=2.6, **_):
     def cost(X):
-        mean, std = gp.predict(X.reshape(-1, 1), return_std=True)
+        mean, std = gp.predict(X.reshape(1, -1), return_std=True)
         return mean - kappa * std
 
     return cost
