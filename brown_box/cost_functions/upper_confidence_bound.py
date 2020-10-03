fded5bf79b8c55359357772d6552654403656926
Jakub Klus <jakub.klus@innovatrics.com>
2020-10-03 09:59:20 +0000

    Solve dimension problem for cost_functions

--- brown_box/cost_functions/upper_confidence_bound.py
+++ brown_box/cost_functions/upper_confidence_bound.py
@@ -9,7 +9,9 @@ def ucb(gp, tr, kappa=2.6, **_):
 
 def ucb_real(gp, tr, kappa=2.6, **_):
     def cost(X):
-        mean, std = gp.predict(X.reshape(1, -1), return_std=True)
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
+        mean, std = gp.predict(X, return_std=True)
         return mean - kappa * std
 
     return cost
