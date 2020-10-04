ef7be953641a328fd9d30a7c1d7d49d522c22d8f
Pavel Grunt <pavel.grunt@gmail.com>
2020-10-04 09:09:26 +0200

    Use BrownBoxAbstractOptimizer to test random search based solutions

--- brown_box/optimizers/brown_box_abstract_optimizer.py
+++ brown_box/optimizers/brown_box_abstract_optimizer.py
@@ -1,16 +1,53 @@
 import numpy as np
 
+import bayesmark.random_search as rs
+from bayesmark import np_util
 from bayesmark.abstract_optimizer import AbstractOptimizer
+
+from ..utils import HyperTransformer
 
 
 class BrownBoxAbstractOptimizer(AbstractOptimizer):
     """Base class for Brown Box optimizers"""
 
-    def __init__(self, api_config):
+    def __init__(self, api_config, random=np_util.random):
         super().__init__(api_config)
+
+        self._random_state = random
+        self.tr = HyperTransformer(api_config)
 
         self.known_points = []
         self.known_values = []
+
+    def random_suggestion(self, n_suggestions):
+        x_guess = rs.suggest_dict(
+            [],
+            [],
+            self.api_config,
+            n_suggestions=n_suggestions,
+            random=self._random_state,
+        )
+        return x_guess
+
+    def suggest(self, n_suggestions=1):
+        """Make `n_suggestions` suggestions for what to evaluate next.
+
+        This requires the user observe all previous suggestions before calling
+        again.
+
+        Parameters
+        ----------
+        n_suggestions : int
+            The number of suggestions to return.
+
+        Returns
+        -------
+        next_guess : list of dict
+            List of `n_suggestions` suggestions to evaluate the objective
+            function. Each suggestion is a dictionary where each key
+            corresponds to a parameter being optimized.
+        """
+        return self.random_suggestion(n_suggestions)
 
     def observe(self, X, y):
         """Feed the observations back to hyperopt.
