5ba4cf063d3dcf9155967e5d447b84f7253dbaa9
Jakub Klus <jakub.klus@innovatrics.com>
2020-10-05 20:13:20 +0000

    Rework quantization and coercion
     Utilizing proper rounding instead of retyping.
     Removed qlogit since it works only in range (0,1)
     logit of 0 or 1 returns -inf or inf.

--- brown_box/utils/__init__.py
+++ brown_box/utils/__init__.py
@@ -1,14 +1,12 @@
 from .discrete_kernel import DiscreteKernel
 from .hyper_transformer import HyperTransformer
-from .qops import qbiexp, qbilog, qexp10, qexpit, qlog10, qlogit
+from .qops import qbiexp, qbilog, qexp10, qlog10
 
 __all__=[
     "qbiexp",
     "qbilog",
     "qexp10",
-    "qexpit",
     "qlog10",
-    "qlogit",
     "HyperTransformer",
     "DiscreteKernel",
 ]
