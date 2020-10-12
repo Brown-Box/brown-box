765061170a95266500e393304fff7c789fee9dac
Jakub Klus <jakub.klus@innovatrics.com>
2020-10-12 12:32:28 +0000

    Fixed EI and POI

--- brown_box/cost_functions/expected_improvement.py
+++ brown_box/cost_functions/expected_improvement.py
@@ -1,11 +1,11 @@
 from scipy.stats import norm
 
 
-def ei(gp, tr, min_y=0, xi=0.1, **_):
+def ei(gp, tr, min_y=0, xi=0.01, **_):
     def cost(**kwargs):
         X = tr.to_real_space(**kwargs)
         mean, std = gp.predict(X, return_std=True)
-        a = mean + min_y + xi
+        a = mean - min_y - xi
         z = a / std
         return a * norm.cdf(z) - std * norm.pdf(z)
 
@@ -17,7 +17,7 @@ def ei_real(gp, tr, min_y=0, xi=0.01, **_):
         if X.ndim == 1:
             X = X.reshape(1, -1)
         mean, std = gp.predict(X, return_std=True)
-        a = mean + min_y + xi
+        a = mean - min_y - xi
         z = a / std
         return a * norm.cdf(z) - std * norm.pdf(z)
 
--- brown_box/cost_functions/probability_of_improvement.py
+++ brown_box/cost_functions/probability_of_improvement.py
@@ -1,21 +1,21 @@
 from scipy.stats import norm
 
 
-def poi(gp, tr, min_y=0, xi=0.1, **_):
+def poi(gp, tr, min_y=0, xi=0.01, **_):
     def cost(**kwargs):
         X = tr.to_real_space(**kwargs)
         mean, std = gp.predict(X, return_std=True)
-        z = (mean + min_y + xi) / std
+        z = (mean - min_y - xi) / std
         return norm.cdf(z)
 
     return cost
 
-def poi_real(gp, tr, min_y=0, xi=0.1, **_):
+def poi_real(gp, tr, min_y=0, xi=0.01, **_):
     def cost(X):
         if X.ndim == 1:
             X = X.reshape(1, -1)
         mean, std = gp.predict(X, return_std=True)
-        z = (mean + min_y + xi) / std
+        z = (mean - min_y - xi) / std
         return norm.cdf(z)
 
     return cost
