b273f97b838dbe48e68c6b244b629c7b6aa078fa
MachichM <martindobrovolny@seznam.cz>
2020-10-12 15:08:44 +0200

    Remove GeneticSearch

--- brown_box/optimizers/__init__.py
+++ brown_box/optimizers/__init__.py
@@ -1,13 +1,11 @@
 from .multi_gp import MultiGaussianProcess
 from .markov_gp import MarkovGaussianProcess
 from .markov_gp_real import MarkovGaussianProcessReal
-from .genetic_algorithm_gp_real import GAMarkovGaussianProcessReal
 from .brown_box_abstract_optimizer import BrownBoxAbstractOptimizer
 
 __all__ = [
     "MultiGaussianProcess",
     "MarkovGaussianProcess",
     "MarkovGaussianProcessReal",
-    "GAMarkovGaussianProcessReal",
     "BrownBoxAbstractOptimizer",
 ]
