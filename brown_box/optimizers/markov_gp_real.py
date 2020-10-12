1eb0c57b17c76574a9a857d189f3506d7ad3520a
Jakub Klus <jakub.klus@innovatrics.com>
2020-10-12 10:05:09 +0000

    Add init_steps to optimizer core

--- brown_box/optimizers/markov_gp_real.py
+++ brown_box/optimizers/markov_gp_real.py
@@ -24,7 +24,8 @@ class MarkovGaussianProcessReal(BrownBoxAbstractOptimizer):
         xi=5.0,
         r_xi=1,
         kappa=2.6,
-        iter_timeout=40.0
+        iter_timeout=40.0,
+        min_known=2,
     ):
         """This optimizes samples multiple suggestions from Gaussian Process.
 
@@ -44,6 +45,7 @@ class MarkovGaussianProcessReal(BrownBoxAbstractOptimizer):
         self.r_xi = r_xi
         self.kappa = kappa
         self._iter = 0
+        self.min_known = min_known
 
     def suggest(self, n_suggestions=1):
         """Make `n_suggestions` suggestions for what to evaluate next.
@@ -63,7 +65,7 @@ class MarkovGaussianProcessReal(BrownBoxAbstractOptimizer):
             function. Each suggestion is a dictionary where each key
             corresponds to a parameter being optimized.
         """
-        if len(self.known_points) < 20:
+        if len(self.known_points) < self.min_known:
             return self.random_suggestion(n_suggestions)
 
         new_points = []
@@ -89,6 +91,7 @@ class MarkovGaussianProcessReal(BrownBoxAbstractOptimizer):
             meta_minimizer = self._meta_optimizer(
                 self.tr, self._random_state, cost_f, step=self._iter
             )
+            # meta_minimizer.observe(self.known_points, self.known_values)
             meta_minimizer.observe(all_points, all_values)
             min_point = [meta_minimizer.suggest(timeout=self.iter_timeout*0.9/n_suggestions)]
             new_points += min_point
@@ -103,14 +106,6 @@ class MarkovGaussianProcessReal(BrownBoxAbstractOptimizer):
         return new_points
 
     def _gp(self):
-        # length_scale = (ubs-lbs)/(16+self._iter)
-        # if self._iter < 5:
-        #     lbs = np.asarray(self.tr._lb)
-        #     ubs = np.asarray(self.tr._ub)
-        #     length_scale = (ubs-lbs)/16
-        # else:
-        #     length_scale = 1.0
-        # self.kernel.length_scale = length_scale
         return GaussianProcessRegressor(
             kernel=DiscreteKernel(self.kernel, self.tr),
             # kernel=self.kernel,
