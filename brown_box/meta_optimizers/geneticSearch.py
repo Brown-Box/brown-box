06b623e726b18d9af441bdc6c78f05f608f98529
MachichM <martindobrovolny@seznam.cz>
2020-10-12 16:36:04 +0200

    Revert "Remove GeneticSearch"
    This reverts commit b273f97b838dbe48e68c6b244b629c7b6aa078fa.

--- brown_box/meta_optimizers/geneticSearch.py
+++ brown_box/meta_optimizers/geneticSearch.py
@@ -13,6 +13,118 @@ from scipy.optimize import Bounds
 from ..utils import HyperTransformer, spec_to_bound
 
 
+class GeneticSearch:
+    def __init__(self, transformer: HyperTransformer) -> None:
+        self._transformer = transformer
+        self._api_config = transformer.api_config
+        self._start_time = None
+        self._timeout = None
+        self._timeout_passed = None
+
+    def suggest(
+        self, fit_function: callable, seed: int = 42, timeout: Optional[int] = None
+    ) -> dict:
+        bounds = []
+        for param_name, param_info in self._api_config.items():
+            if param_info["type"] in ["real", "int"]:
+                if "range" in param_info:
+                    real_range = spec_to_bound(param_info)
+                    ga_bound_value = (real_range[0][0][()], real_range[1][0][()])
+                else:
+                    ga_bound_value = (0, 1)
+            elif param_info["type"] in ["cat", "bool"]:
+                ga_bound_value = (0, 1)
+            else:
+                param_type = param_info["type"]
+                raise ValueError(f"Unknown parameter type {param_type}")
+
+            bounds.append(ga_bound_value)
+
+        if timeout:
+            self._start_time = time()
+            self._timeout = timeout
+            callback = self._timeout_callback
+            self._timeout_passed = False
+        else:
+            callback = None
+
+        func = self._GA_to_real_wrapper(fit_function)
+
+        # TODO turn on polish? Find out our own way to polish result?
+        solver = BrownEvolutionSolver(
+            transformer=self._transformer,
+            func=func,
+            bounds=bounds,
+            seed=seed,
+            callback=callback,
+            polish=False,
+        )
+        solver_return_value = solver.solve()
+        if not solver_return_value["success"] and not self._timeout_passed:
+            message = solver_return_value["message"]
+            raise ValueError(
+                f"DifferentialEvolutionSolver failed with message: {message}"
+            )
+
+        best_value = solver.x
+        real_params = self._GA_to_real(best_value)
+        if len(real_params.shape) == 1:
+            real_params = np.array([real_params])
+        result = self._transformer.to_hyper_space(real_params)
+        result = {
+            key: val[0][0] if type(val) is np.ndarray else val[0]
+            for key, val in result.items()
+        }
+
+        return result
+
+    def _timeout_callback(self, xk, convergence):
+        if time() - self._start_time >= self._timeout:
+            self._timeout_passed = True
+            return True
+        else:
+            return False
+
+    def _GA_to_real_wrapper(self, func):
+        def transform(population_member):
+            real_params = self._GA_to_real(population_member)
+            return func(real_params)
+
+        return transform
+
+    def _GA_to_real(self, population_member):
+        real_params = []
+        for i, (param_name, param_info) in enumerate(self._api_config.items()):
+            if param_info["type"] in ["real", "int"]:
+                if "range" in param_info:
+                    value = np.array([population_member[i]])
+                else:
+                    number_of_categories = len(param_info["values"])
+                    cat_pos = min(
+                        int(population_member[i] * number_of_categories),
+                        number_of_categories - 1,
+                    )
+                    hypervalue = param_info["values"][cat_pos]
+                    value = self._transformer._reals[param_name]([hypervalue])
+            elif param_info["type"] == "cat":
+                number_of_categories = len(param_info["values"])
+                cat_pos = min(
+                    int(population_member[i] * number_of_categories),
+                    number_of_categories - 1,
+                )
+                hypervalue = param_info["values"][cat_pos]
+                value = self._transformer._reals[param_name]([hypervalue])[0]
+            elif param_info["type"] == "bool":
+                hypervalue = int(population_member[i] >= 0.5)
+                value = self._transformer._reals[param_name]([hypervalue])
+            else:
+                raise ValueError()
+            real_params.append(value)
+
+        real_params = np.concatenate(real_params)
+        return real_params
+
+
 class GeneticSearchNonRandom:
     def __init__(
         self, transformer: HyperTransformer, random, cost_function, step=0
@@ -98,14 +210,15 @@ class GeneticSearchNonRandom:
         self.top_points_real = X
 
 
-_status_message = {
-    "success": "Optimization terminated successfully.",
-    "maxfev": "Maximum number of function evaluations has " "been exceeded.",
-    "maxiter": "Maximum number of iterations has been " "exceeded.",
-    "timeout": "Timeout reached.",
-    "pr_loss": "Desired error not necessarily achieved due " "to precision loss.",
-    "nan": "NaN result encountered.",
-}
+_status_message = {'success': 'Optimization terminated successfully.',
+                   'maxfev': 'Maximum number of function evaluations has '
+                              'been exceeded.',
+                   'maxiter': 'Maximum number of iterations has been '
+                              'exceeded.',
+                   'timeout': 'Timeout reached.',
+                   'pr_loss': 'Desired error not necessarily achieved due '
+                              'to precision loss.',
+                   'nan': 'NaN result encountered.'}
 
 
 class BrownEvolutionSolver(DifferentialEvolutionSolver):
---
+++ brown_box/optimizers/genetic_algorithm_gp_real.py
@@ -0,0 +1,87 @@
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern
+
+from bayesmark import np_util
+from bayesmark.experiment import experiment_main
+
+from ..cost_functions import ucb_real
+from ..meta_optimizers.geneticSearch import GeneticSearch
+from ..utils import DiscreteKernel
+from .markov_gp_real import MarkovGaussianProcessReal
+
+
+class GAMarkovGaussianProcessReal(MarkovGaussianProcessReal):
+    primary_import = "bayesmark"
+
+    def __init__(
+        self,
+        api_config,
+        random=np_util.random,
+        meta_optimizer=GeneticSearch,
+        cost=ucb_real,
+    ):
+        super().__init__(api_config, random, meta_optimizer, cost)
+
+    def suggest(self, n_suggestions=1):
+        """Make `n_suggestions` suggestions for what to evaluate next.
+
+        This requires the user observe all previous suggestions before calling
+        again.
+
+        Parameters
+        ----------
+        n_suggestions : int
+            The number of suggestions to return.
+
+        Returns
+        -------
+        next_guess : list of dict
+            List of `n_suggestions` suggestions to evaluate the objective
+            function. Each suggestion is a dictionary where each key
+            corresponds to a parameter being optimized.
+        """
+        if len(self.known_points) < 2:
+            return self.random_suggestion(n_suggestions)
+
+        new_points = []
+        new_values = []
+        for k in range(n_suggestions):
+            gp = GaussianProcessRegressor(
+                kernel=DiscreteKernel(Matern(nu=2.5), self.tr),
+                alpha=1e-6,
+                normalize_y=True,
+                n_restarts_optimizer=5,
+                random_state=self._random_state,
+            )
+            all_points = self.known_points[:]
+            all_points += new_points
+            all_values = np.concatenate([self.known_values, new_values])
+            all_known_points = {
+                k: [dic[k] for dic in all_points] for k in all_points[0]
+            }
+            gp.fit(self.tr.to_real_space(**all_known_points), all_values)
+
+            cost_f = self._cost(
+                gp,
+                self.tr,
+                max_y=max(all_values),
+                min_y=min(all_values),
+                xi=0.1,
+                kappa=2.6,
+            )
+            meta_minimizer = self._meta_optimizer(self.tr)
+
+            min_point = [meta_minimizer.suggest(cost_f, k, timeout=4.0)]
+            new_points += min_point
+
+            _p = {k: [dic[k] for dic in min_point] for k in min_point[0]}
+            X = self.tr.to_real_space(**_p)
+            min_value = gp.predict(X)[0]
+            new_values.append(min_value)
+            # print(min_value, min_point)
+        return new_points
+
+
+if __name__ == "__main__":
+    experiment_main(GAMarkovGaussianProcessReal)
